0e95a452a9facad726f5762be5c0e47370cf1e3f
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-12-12 15:01:06 +1100

    Update work_with_sh.py

--- devc/backend/work_with_sh.py
+++ devc/backend/work_with_sh.py
@@ -7,3 +7,10 @@ def _get_output(command:str):
 
 def get_processor():
   processor_info = _get_output('lscpu')
+  processor_name = processor_info.split('Model name:')[1].split('\n')[0]
+  processor_vendor = processor_info.split('Vendor ID:')[1].split('\n')[0]
+  return {
+    "name": processor_name,
+    "vendor": processor_vendor,
+    "original": processor_info
+  }
