a1103d08ed75f47bf8513673978dc41b129ed210
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-12-12 14:57:07 +1100

    Update work_with_sh.py

--- devc/backend/work_with_sh.py
+++ devc/backend/work_with_sh.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python
+import subprocess
 
+def _get_output(command:str):
+  output = subprocess.run(command.split(' '), capture_output=True, text=True)
+  return output.stdout
+
+def get_processor():
+  processor_info = _get_output('lscpu')
