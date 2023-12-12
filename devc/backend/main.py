8db4b019f4512a05bd94daba6394844f3b775b92
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-12-12 15:30:30 +1100

    Update main.py

--- devc/backend/main.py
+++ devc/backend/main.py
@@ -1,4 +1,14 @@
 from work_with_sh import get_processor
 from config import *
+from flask import Flask
 
 print("[*][DEVCMGR*] Backend is running.")
+print("[*][DEVCMGR*] Hosting server...")
+
+class WSGI(Flask):
+  def __init__(self):
+    super().__init__('devcmgrhost')
+
+    @self.route('/')
+    def route():
+      return "HAHA"
